f4d5c963e1f51588c0a207e5b0f8bf4d84ae8da4
garciaarthur-cell <garcia.arthur@escola.pr.gov.br>
2026-06-29 19:03:29 -0300

    alterações de acessibilidade

--- script.js
+++ script.js
@@ -15,3 +15,21 @@ function verificar() {
         alert(nome + ", resposta errada");
     }
 }
+function alternarModo() {
+    const body = document.body;
+    body.classList.toggle('dark'); // Alterna as cores usando o arquivo style.css
+    
+    // Guarda na memória do navegador se o usuário prefere o tema escuro ou claro
+    if (body.classList.contains('dark')) {
+        localStorage.setItem('tema', 'escuro');
+    } else {
+        localStorage.setItem('tema', 'claro');
+    }
+}
+
+// Carrega as preferências guardadas assim que a página abre
+window.onload = function() {
+    if (localStorage.getItem('tema') === 'escuro') {
+        document.body.classList.add('dark');
+    }
+};
